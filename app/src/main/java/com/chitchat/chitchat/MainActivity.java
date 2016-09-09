09d7f0c100f4e6f174327f617e2a655eeb4a0ed0
Korirmitchelle <korirmitchelle@gmail.com>
2016-09-09 13:08:38 +0300

    navigation view

--- app/src/main/java/com/chitchat/chitchat/MainActivity.java
+++ app/src/main/java/com/chitchat/chitchat/MainActivity.java
@@ -1,17 +1,23 @@
 package com.chitchat.chitchat;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupToolbar();
+        setupNavbar();
     }
     private void setupToolbar(){
         Toolbar tool =(Toolbar) findViewById(R.id.toolbar);
@@ -26,4 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void setupNavbar(){
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
