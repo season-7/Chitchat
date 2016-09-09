4c7a775bef483508ef550b761c681b869663785f
Korirmitchelle <korirmitchelle@gmail.com>
2016-09-09 09:01:54 +0300

    menu  ,toolbar and color schemes

--- app/src/main/java/com/chitchat/chitchat/MainActivity.java
+++ app/src/main/java/com/chitchat/chitchat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.chitchat.chitchat;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+    }
+    private void setupToolbar(){
+        Toolbar tool =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tool);
+
+        /*To show menu icon*/
+        final ActionBar one=getSupportActionBar();
+        one.setHomeAsUpIndicator(R.drawable.menu);
+        one.setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 }
