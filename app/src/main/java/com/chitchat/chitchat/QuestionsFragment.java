1d0ba411c20998e88e6d48fb14f7fd4b52f1adc6
EduhG <edwinnyangena@gmail.com>
2016-09-12 13:20:41 +0300

    Created questions fragment

---
+++ app/src/main/java/com/chitchat/chitchat/QuestionsFragment.java
@@ -0,0 +1,34 @@
+package com.chitchat.chitchat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class QuestionsFragment extends Fragment {
+    public QuestionsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final Button mainButton = (Button) rootView.findViewById(R.id.main_button);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mainButton.setBackgroundColor(getContext().getResources().getColor(R.color.colorAccent));
+            }
+        });
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+}
